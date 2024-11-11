11e0c6b33a59f09b8a5c6165afe2904a82196cb0
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2024-11-11 17:46:36 +0900

    Add save message to Save Parking Location page

--- app/src/main/java/com/example/parklog/ParkingLocationActivity.kt
+++ app/src/main/java/com/example/parklog/ParkingLocationActivity.kt
@@ -5,42 +5,50 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class ParkingLocationActivity : AppCompatActivity() {
 
-    private lateinit var photoUri: Uri // 사진 URI를 저장하는 변수
+    private lateinit var photoUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parking_location)
 
         val homeButton: ImageButton = findViewById(R.id.homeButton)
+        val saveButton: Button = findViewById(R.id.btnSave)
+        val locationEditText: EditText = findViewById(R.id.etLocationDescription)
+        val feeEditText: EditText = findViewById(R.id.etFee)
+        val savePhotoText: ImageView = findViewById(R.id.savePhotoText)
 
-        // 카메라 촬영 결과를 받을 콜백 설정
         val takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
             if (success) {
-                findViewById<ImageView>(R.id.savePhotoText).setImageURI(photoUri) // 촬영된 사진 URI로 이미지 뷰 업데이트
+                savePhotoText.setImageURI(photoUri)
             }
         }
 
-        // 카메라 버튼 클릭 시 사진 촬영 시작
         findViewById<ImageButton>(R.id.btnCamera).setOnClickListener {
-            photoUri = createImageFileUri() // URI 생성
-            takePictureLauncher.launch(photoUri) // 사진 촬영 시작
+            photoUri = createImageFileUri()
+            takePictureLauncher.launch(photoUri)
         }
 
-        // 홈 버튼 클릭 시 메인 화면으로 이동
+        saveButton.setOnClickListener {
+
+            Toast.makeText(this, "Successfully saved!", Toast.LENGTH_SHORT).show()
+            startActivity(intent) // List 페이지로 이동
+        }
+
         homeButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 
-    // 사진을 저장할 파일의 URI 생성 함수
     private fun createImageFileUri(): Uri {
         val contentValues = ContentValues().apply {
             put(MediaStore.Images.Media.DISPLAY_NAME, "captured_photo.jpg")
